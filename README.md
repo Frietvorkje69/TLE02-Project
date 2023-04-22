7805eb43759aea0282848244d186d7cbc7756872
Kevin <89836835+Kaasstengel28@users.noreply.github.com>
2023-04-22 03:14:34 +0200

    Barebones login systeem, nog geen log uit of database

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # TLE02-Project
+bruhbabingo
+scrabagobo
+kinko konko
+bruhsibroski
---
+++ passportConfig.js
@@ -0,0 +1,32 @@
+const localStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
+
+
+function initialize(passport, getUserByEmail, getUserById){
+    //authenticatie
+    const authenticateUsers = async (email, password, done) => {
+
+        const user = getUserByEmail(email)
+        if (user == null) {
+            return done(null, false, {message: "no user met email"})
+        }
+        try {
+            if(await bcrypt.compare(password, user.password)) {
+                return done(null, user)
+            } else {
+                return done (null, false, {message: "password welloe"})
+            }
+        } catch (e) {
+            console.log(e)
+            return done(e);
+        }
+    }
+
+    passport.use(new localStrategy({usernameField: 'email'}, authenticateUsers))
+    passport.serializeUser((user, done) => done(null, user.id))
+    passport.deserializeUser((id, done) => {
+        return done(null, getUserById(id))
+    })
+}
+
+module.exports = initialize
